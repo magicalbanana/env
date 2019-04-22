201d6bc24fe45c0d792250132ff381633e70afe0
Kareem Hepburn <kareemgan@gmail.com>
2019-04-21 22:37:14 -0500

    Enhancements

--- env.go
+++ env.go
@@ -1,30 +1,78 @@
 package env
 
 import (
+	"encoding"
 	"errors"
 	"fmt"
 	"os"
 	"reflect"
 	"strconv"
 	"strings"
-	"time"
 )
 
+// nolint: gochecknoglobals
 var (
 	// ErrNotAStructPtr is returned if you pass something that is not a pointer to a
 	// Struct to Parse
-	ErrNotAStructPtr = errors.New("Expected a pointer to a Struct")
-	// ErrUnsupportedType if the struct field type is not supported by env
-	ErrUnsupportedType = errors.New("Type is not supported")
-	// ErrUnsupportedSliceType if the slice element type is not supported by env
-	ErrUnsupportedSliceType = errors.New("Unsupported slice type")
-	// Friendly names for reflect types
-	sliceOfInts     = reflect.TypeOf([]int(nil))
-	sliceOfInt64s   = reflect.TypeOf([]int64(nil))
-	sliceOfStrings  = reflect.TypeOf([]string(nil))
-	sliceOfBools    = reflect.TypeOf([]bool(nil))
-	sliceOfFloat32s = reflect.TypeOf([]float32(nil))
-	sliceOfFloat64s = reflect.TypeOf([]float64(nil))
+	ErrNotAStructPtr = errors.New("env: expected a pointer to a Struct")
+	// OnEnvVarSet is an optional convenience callback, such as for logging purposes.
+	// If not nil, it's called after successfully setting the given field from the given value.
+	OnEnvVarSet func(reflect.StructField, string)
+
+	defaultBuiltInParsers = map[reflect.Kind]ParserFunc{
+		reflect.Bool: func(v string) (interface{}, error) {
+			return strconv.ParseBool(v)
+		},
+		reflect.String: func(v string) (interface{}, error) {
+			return v, nil
+		},
+		reflect.Int: func(v string) (interface{}, error) {
+			i, err := strconv.ParseInt(v, 10, 32)
+			return int(i), err
+		},
+		reflect.Int16: func(v string) (interface{}, error) {
+			i, err := strconv.ParseInt(v, 10, 16)
+			return int16(i), err
+		},
+		reflect.Int32: func(v string) (interface{}, error) {
+			i, err := strconv.ParseInt(v, 10, 32)
+			return int32(i), err
+		},
+		reflect.Int64: func(v string) (interface{}, error) {
+			return strconv.ParseInt(v, 10, 64)
+		},
+		reflect.Int8: func(v string) (interface{}, error) {
+			i, err := strconv.ParseInt(v, 10, 8)
+			return int8(i), err
+		},
+		reflect.Uint: func(v string) (interface{}, error) {
+			i, err := strconv.ParseUint(v, 10, 32)
+			return uint(i), err
+		},
+		reflect.Uint16: func(v string) (interface{}, error) {
+			i, err := strconv.ParseUint(v, 10, 16)
+			return uint16(i), err
+		},
+		reflect.Uint32: func(v string) (interface{}, error) {
+			i, err := strconv.ParseUint(v, 10, 32)
+			return uint32(i), err
+		},
+		reflect.Uint64: func(v string) (interface{}, error) {
+			i, err := strconv.ParseUint(v, 10, 64)
+			return i, err
+		},
+		reflect.Uint8: func(v string) (interface{}, error) {
+			i, err := strconv.ParseUint(v, 10, 8)
+			return uint8(i), err
+		},
+		reflect.Float64: func(v string) (interface{}, error) {
+			return strconv.ParseFloat(v, 64)
+		},
+		reflect.Float32: func(v string) (interface{}, error) {
+			f, err := strconv.ParseFloat(v, 32)
+			return float32(f), err
+		},
+	}
 )
 
 // CustomParsers is a friendly name for the type that `ParseWithFuncs()` accepts
@@ -36,15 +84,7 @@ type ParserFunc func(v string) (interface{}, error)
 // Parse parses a struct containing `env` tags and loads its values from
 // environment variables.
 func Parse(v interface{}) error {
-	ptrRef := reflect.ValueOf(v)
-	if ptrRef.Kind() != reflect.Ptr {
-		return ErrNotAStructPtr
-	}
-	ref := ptrRef.Elem()
-	if ref.Kind() != reflect.Struct {
-		return ErrNotAStructPtr
-	}
-	return doParse(ref, make(map[reflect.Type]ParserFunc, 0))
+	return ParseWithFuncs(v, CustomParsers{})
 }
 
 // ParseWithFuncs is the same as `Parse` except it also allows the user to pass
@@ -63,33 +103,38 @@ func ParseWithFuncs(v interface{}, funcMap CustomParsers) error {
 
 func doParse(ref reflect.Value, funcMap CustomParsers) error {
 	refType := ref.Type()
-	var errorList []string
 
 	for i := 0; i < refType.NumField(); i++ {
-		if reflect.Ptr == ref.Field(i).Kind() && !ref.Field(i).IsNil() && ref.Field(i).CanSet() {
-			err := Parse(ref.Field(i).Interface())
+		refField := ref.Field(i)
+		if reflect.Ptr == refField.Kind() && !refField.IsNil() && refField.CanSet() {
+			err := Parse(refField.Interface())
 			if nil != err {
 				return err
 			}
 			continue
 		}
-		value, err := get(refType.Field(i))
+		refTypeField := refType.Field(i)
+		value, err := get(refTypeField)
 		if err != nil {
-			errorList = append(errorList, err.Error())
-			continue
+			return err
 		}
 		if value == "" {
+			if reflect.Struct == refField.Kind() {
+				if err := doParse(refField, funcMap); err != nil {
+					return err
+				}
+			}
 			continue
 		}
-		if err := set(ref.Field(i), refType.Field(i), value, funcMap); err != nil {
-			errorList = append(errorList, err.Error())
-			continue
+		if err := set(refField, refTypeField, value, funcMap); err != nil {
+			return err
+		}
+		// TODO: change this to a param instead of global
+		if OnEnvVarSet != nil {
+			OnEnvVarSet(refTypeField, value)
 		}
 	}
-	if len(errorList) == 0 {
-		return nil
-	}
-	return errors.New(strings.Join(errorList, ". "))
+	return nil
 }
 
 func get(field reflect.StructField) (string, error) {
@@ -103,6 +148,11 @@ func get(field reflect.StructField) (string, error) {
 	defaultValue := field.Tag.Get("envDefault")
 	val = getOr(key, defaultValue)
 
+	expandVar := field.Tag.Get("envExpand")
+	if strings.ToLower(expandVar) == "true" {
+		val = os.ExpandEnv(val)
+	}
+
 	if len(opts) > 0 {
 		for _, opt := range opts {
 			// The only option supported is "required".
@@ -112,7 +162,7 @@ func get(field reflect.StructField) (string, error) {
 			case "required":
 				val, err = getRequired(key)
 			default:
-				err = errors.New("Env tag option " + opt + " not supported.")
+				err = fmt.Errorf("env: tag option %q not supported", opt)
 			}
 		}
 	}
@@ -130,8 +180,7 @@ func getRequired(key string) (string, error) {
 	if value, ok := os.LookupEnv(key); ok {
 		return value, nil
 	}
-	// We do not use fmt.Errorf to avoid another import.
-	return "", errors.New("Required environment variable " + key + " is not set")
+	return "", fmt.Errorf(`env: required environment variable "%q" is not set`, key)
 }
 
 func getOr(key, defaultValue string) string {
@@ -142,213 +191,138 @@ func getOr(key, defaultValue string) string {
 	return defaultValue
 }
 
-func set(field reflect.Value, refType reflect.StructField, value string, funcMap CustomParsers) error {
-	switch field.Kind() {
-	case reflect.Slice:
-		separator := refType.Tag.Get("envSeparator")
-		return handleSlice(field, value, separator)
-	case reflect.String:
-		parserFunc, ok := funcMap[refType.Type]
-		if ok {
-			val, err := parserFunc(value)
-			if err != nil {
-				return err
-			}
-			field.Set(reflect.ValueOf(val))
-		} else {
+func set(field reflect.Value, sf reflect.StructField, value string, funcMap CustomParsers) error {
+	if field.Kind() == reflect.Slice {
+		return handleSlice(field, value, sf, funcMap)
+	}
 
-			field.SetString(value)
-		}
-	case reflect.Bool:
-		bvalue, err := strconv.ParseBool(value)
-		if err != nil {
-			return err
-		}
-		field.SetBool(bvalue)
-	case reflect.Int:
-		intValue, err := strconv.ParseInt(value, 10, 32)
-		if err != nil {
-			return err
-		}
-		field.SetInt(intValue)
-	case reflect.Uint:
-		uintValue, err := strconv.ParseUint(value, 10, 32)
-		if err != nil {
-			return err
-		}
-		field.SetUint(uintValue)
-	case reflect.Float32:
-		v, err := strconv.ParseFloat(value, 32)
-		if err != nil {
-			return err
-		}
-		field.SetFloat(v)
-	case reflect.Float64:
-		v, err := strconv.ParseFloat(value, 64)
-		if err != nil {
-			return err
-		}
-		field.Set(reflect.ValueOf(v))
-	case reflect.Int64:
-		if refType.Type.String() == "time.Duration" {
-			dValue, err := time.ParseDuration(value)
-			if err != nil {
-				return err
-			}
-			field.Set(reflect.ValueOf(dValue))
-		} else {
-			intValue, err := strconv.ParseInt(value, 10, 64)
-			if err != nil {
-				return err
-			}
-			field.SetInt(intValue)
-		}
-	case reflect.Struct:
-		return handleStruct(field, refType, value, funcMap)
-	default:
-		parserFunc, ok := funcMap[refType.Type]
-		if !ok {
-			return ErrUnsupportedType
-		}
+	parserFunc, ok := funcMap[sf.Type]
+	if ok {
 		val, err := parserFunc(value)
 		if err != nil {
-			return err
+			return newParseError(sf, err)
 		}
 		field.Set(reflect.ValueOf(val))
+		return nil
 	}
-	return nil
+
+	parserFunc, ok = defaultBuiltInParsers[sf.Type.Kind()]
+	if ok {
+		val, err := parserFunc(value)
+		if err != nil {
+			return newParseError(sf, err)
+		}
+		field.Set(reflect.ValueOf(val).Convert(sf.Type))
+		return nil
+	}
+
+	return handleTextUnmarshaler(field, value, sf)
 }
 
-func handleStruct(field reflect.Value, refType reflect.StructField, value string, funcMap CustomParsers) error {
-	// Does the custom parser func map contain this type?
-	parserFunc, ok := funcMap[field.Type()]
-	if !ok {
-		// Map does not contain a custom parser for this type
-		return ErrUnsupportedType
-	}
-
-	// Call on the custom parser func
-	data, err := parserFunc(value)
-	if err != nil {
-		return fmt.Errorf("Custom parser error: %v", err)
-	}
-
-	// Set the field to the data returned by the customer parser func
-	rv := reflect.ValueOf(data)
-	field.Set(rv)
-
-	return nil
-}
-
-func handleSlice(field reflect.Value, value, separator string) error {
+func handleSlice(field reflect.Value, value string, sf reflect.StructField, funcMap CustomParsers) error {
+	var separator = sf.Tag.Get("envSeparator")
 	if separator == "" {
 		separator = ","
 	}
+	var parts = strings.Split(value, separator)
 
-	splitData := strings.Split(value, separator)
-
-	switch field.Type() {
-	case sliceOfStrings:
-		field.Set(reflect.ValueOf(splitData))
-	case sliceOfInts:
-		intData, err := parseInts(splitData)
-		if err != nil {
-			return err
-		}
-		field.Set(reflect.ValueOf(intData))
-	case sliceOfInt64s:
-		int64Data, err := parseInt64s(splitData)
-		if err != nil {
-			return err
-		}
-		field.Set(reflect.ValueOf(int64Data))
-
-	case sliceOfFloat32s:
-		data, err := parseFloat32s(splitData)
-		if err != nil {
-			return err
-		}
-		field.Set(reflect.ValueOf(data))
-	case sliceOfFloat64s:
-		data, err := parseFloat64s(splitData)
-		if err != nil {
-			return err
-		}
-		field.Set(reflect.ValueOf(data))
-	case sliceOfBools:
-		boolData, err := parseBools(splitData)
-		if err != nil {
-			return err
-		}
-		field.Set(reflect.ValueOf(boolData))
-	default:
-		return ErrUnsupportedSliceType
+	var elemType = sf.Type.Elem()
+	if elemType.Kind() == reflect.Ptr {
+		elemType = elemType.Elem()
 	}
+
+	if _, ok := reflect.New(elemType).Interface().(encoding.TextUnmarshaler); ok {
+		return parseTextUnmarshalers(field, parts, sf)
+	}
+
+	parserFunc, ok := funcMap[elemType]
+	if !ok {
+		parserFunc, ok = defaultBuiltInParsers[elemType.Kind()]
+		if !ok {
+			return newNoParserError(sf)
+		}
+	}
+
+	var result = reflect.MakeSlice(sf.Type, 0, len(parts))
+	for _, part := range parts {
+		r, err := parserFunc(part)
+		if err != nil {
+			return newParseError(sf, err)
+		}
+		var v = reflect.ValueOf(r).Convert(elemType)
+		if sf.Type.Elem().Kind() == reflect.Ptr {
+			// TODO: add this!
+			return fmt.Errorf("env: point slices of built-in and aliased types are not supported: %s %s", sf.Name, sf.Type)
+		}
+		result = reflect.Append(result, v)
+	}
+
+	field.Set(result)
 	return nil
 }
 
-func parseInts(data []string) ([]int, error) {
-	var intSlice []int
-
-	for _, v := range data {
-		intValue, err := strconv.ParseInt(v, 10, 32)
-		if err != nil {
-			return nil, err
+func handleTextUnmarshaler(field reflect.Value, value string, sf reflect.StructField) error {
+	if reflect.Ptr == field.Kind() {
+		if field.IsNil() {
+			field.Set(reflect.New(field.Type().Elem()))
 		}
-		intSlice = append(intSlice, int(intValue))
+	} else if field.CanAddr() {
+		field = field.Addr()
 	}
-	return intSlice, nil
+
+	tm, ok := field.Interface().(encoding.TextUnmarshaler)
+	if !ok {
+		return newNoParserError(sf)
+	}
+	var err = tm.UnmarshalText([]byte(value))
+	return newParseError(sf, err)
 }
 
-func parseInt64s(data []string) ([]int64, error) {
-	var intSlice []int64
-
-	for _, v := range data {
-		intValue, err := strconv.ParseInt(v, 10, 64)
-		if err != nil {
-			return nil, err
+func parseTextUnmarshalers(field reflect.Value, data []string, sf reflect.StructField) error {
+	s := len(data)
+	elemType := field.Type().Elem()
+	slice := reflect.MakeSlice(reflect.SliceOf(elemType), s, s)
+	for i, v := range data {
+		sv := slice.Index(i)
+		kind := sv.Kind()
+		if kind == reflect.Ptr {
+			sv = reflect.New(elemType.Elem())
+		} else {
+			sv = sv.Addr()
 		}
-		intSlice = append(intSlice, int64(intValue))
+		tm := sv.Interface().(encoding.TextUnmarshaler)
+		if err := tm.UnmarshalText([]byte(v)); err != nil {
+			return newParseError(sf, err)
+		}
+		if kind == reflect.Ptr {
+			slice.Index(i).Set(sv)
+		}
 	}
-	return intSlice, nil
+
+	field.Set(slice)
+
+	return nil
 }
 
-func parseFloat32s(data []string) ([]float32, error) {
-	var float32Slice []float32
-
-	for _, v := range data {
-		data, err := strconv.ParseFloat(v, 32)
-		if err != nil {
-			return nil, err
-		}
-		float32Slice = append(float32Slice, float32(data))
+func newParseError(sf reflect.StructField, err error) error {
+	if err == nil {
+		return nil
 	}
-	return float32Slice, nil
+	return parseError{
+		sf:  sf,
+		err: err,
+	}
 }
 
-func parseFloat64s(data []string) ([]float64, error) {
-	var float64Slice []float64
-
-	for _, v := range data {
-		data, err := strconv.ParseFloat(v, 64)
-		if err != nil {
-			return nil, err
-		}
-		float64Slice = append(float64Slice, float64(data))
-	}
-	return float64Slice, nil
+type parseError struct {
+	sf  reflect.StructField
+	err error
 }
 
-func parseBools(data []string) ([]bool, error) {
-	var boolSlice []bool
+func (e parseError) Error() string {
+	return fmt.Sprintf(`env: parse error on field "%s" of type "%s": %v`, e.sf.Name, e.sf.Type, e.err)
+}
 
-	for _, v := range data {
-		bvalue, err := strconv.ParseBool(v)
-		if err != nil {
-			return nil, err
-		}
-
-		boolSlice = append(boolSlice, bvalue)
-	}
-	return boolSlice, nil
+func newNoParserError(sf reflect.StructField) error {
+	return fmt.Errorf(`env: no parser found for field "%s" of type "%s"`, sf.Name, sf.Type)
 }
